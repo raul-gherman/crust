b25f2c3cd48f7de3d7a66f01ff471ae87eb12a61
Raul Gherman <raul.gherman@yandex.com>
2023-10-13 13:31:56 +0300

    enable logging

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 // mod combination;
 mod decoder;
 mod symbol;
-
 use crate::decoder::*;
-
 mod ctrader_open_api;
-
 use tungstenite::connect;
 use url::Url;
+extern crate env_logger;
+use log::info;
 
 fn main() {
     /*
@@ -18,11 +17,17 @@ fn main() {
        }
     */
 
+    std::env::set_var("RUST_LOG", "DEBUG");
+    env_logger::builder()
+        // setting this to None disables the timestamp
+        .format_timestamp(Some(env_logger::TimestampPrecision::Micros))
+        .init();
+
     let server = "wss://demo.ctraderapi.com:5035";
 
     let (mut socket, _response) =
         connect(Url::parse(&server).unwrap()).expect("Can't connect to {server}");
-    println!("Connected to {server}");
+    info!("Connected to {server}");
 
     start(&mut socket);
 }
