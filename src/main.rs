e9d04232d555d1d32dbaecf1b59ebb14c03f2d1f
Raul Gherman <raul.gherman@yandex.com>
2023-12-18 07:33:51 +0200

    add encoder

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
 // mod combination;
 mod decoder;
+mod encoder;
 mod symbol;
 use crate::decoder::*;
+use std::env;
 mod ctrader_open_api;
 use tungstenite::connect;
 use url::Url;
 extern crate env_logger;
-use log::info;
+use log::{error, info};
 
 fn main() {
-    /*
-       let key: &str = "HOME";
-       match env::var(key) {
-           Ok(val) => println!("{key}: {val:?}"),
-           Err(e) => println!("couldn't interpret {key}: {e}"),
-       }
-    */
-
     std::env::set_var("RUST_LOG", "DEBUG");
     env_logger::builder()
         // setting this to None disables the timestamp
         .format_timestamp(Some(env_logger::TimestampPrecision::Micros))
         .init();
 
-    let server = "wss://demo.ctraderapi.com:5035";
+    std::env::set_var("CTRADER_ENDPOINT", "wss://demo.ctraderapi.com:5035");
+    let ctrader_endpoint: &str = "CTRADER_ENDPOINT";
+    match env::var(ctrader_endpoint) {
+        Ok(server) => {
+            info!("{ctrader_endpoint}: {server:?}");
 
-    let (mut socket, _response) =
-        connect(Url::parse(&server).unwrap()).expect("Can't connect to {server}");
-    info!("Connected to {server}");
+            let (mut socket, _response) =
+                connect(Url::parse(&server).unwrap()).expect("Can't connect to {ctrader_endpoint}");
+            info!("Connected to {ctrader_endpoint}");
 
-    start(&mut socket);
+            start(&mut socket);
+        }
+        Err(e) => {
+            error!(
+                "couldn't retrieve ENV value {:?} :: {:?}",
+                &ctrader_endpoint, &e
+            );
+        }
+    }
 }
