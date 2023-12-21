03418ed8eb5c6343d3687f7a77508b5c35b1259e
Raul Gherman <raul.gherman@yandex.com>
2023-12-21 15:52:14 +0200

    prepend cargo clean

---
+++ run.sh
@@ -0,0 +1,7 @@
+cargo clean
+export RUST_LOG="DEBUG"
+export CTRADER_ENDPOINT="wss://demo.ctraderapi.com:5035"
+export ACCESS_TOKEN="sys-ssIRWEUqaU5jjLQ4kqmoXfaazJfVwHM0ls9kcUg"
+export CLIENT_ID="4460_EpyxopCGdxPprbux9hTDaDtaDhfv4T2G0yF1OrnIg7LGYkAXU1"
+export CLIENT_SECRET="C1lpWCK45J9sOTJ0or5b4GBHcxX5Jbt4erLqwOpB3RP8it4Iy0"
+cargo run
