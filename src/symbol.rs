4c192018bbe8c31b978691a990703366f46464eb
Raul Gherman <raul.gherman@yandex.com>
2023-12-21 15:52:00 +0200

    add default values using derivative

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,8 +1,24 @@
 // mod ctrader_open_api;
+use derivative::Derivative;
+
+#[derive(Derivative)]
+#[derivative(Debug, Default)]
 
 pub struct Symbol {
     pub id: i64,
+
+    #[derivative(Default(value = "0"))]
     pub bid: u64,
+
+    #[derivative(Default(value = "0"))]
     pub ask: u64,
+
+    #[derivative(Default(value = "0"))]
     pub spread: u64,
+}
+
+impl Symbol {
+    fn get_spread(ask: u64, bid: u64) -> u64 {
+        ask - bid
+    }
 }
